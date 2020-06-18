91b730ad073292b7b133a89bb3b4bd4be3a56f36
Tina Wang <mwow@yopmail.com>
2020-06-18 21:21:19 +0200

    ADD: matrix product

--- src/views/Airy.vue
+++ src/views/Airy.vue
@@ -5,8 +5,69 @@
       <div style="height: 100vh; line-height: 100vh">
         <span class="view-title">brute</span>
       </div>
-      <v-parallax dark :src="'./assets/images/airy/matrix.jpg'" style="height: 20vw; width: 77vw"></v-parallax>
-      <span class="" ref="subtitle">The Matrix</span>
+      <div class="product-container">
+          <v-parallax :src="'./assets/images/airy/matrix.jpg'" style="height: 22vw; width: 77vw"></v-parallax>
+        <div ref="animated-row-0" class="product-row" style="margin-bottom: -28vw">
+          <div style="display: flex; flex-direction: column; align-items: center">
+            <span class="product-title" style="margin-top: 4vw">The Matrix</span>
+            <span class="product-subtitle" style="margin-top: 2vw">ANALOG AVANT-GARDE</span>
+            <span
+              class="product-desc"
+              style="margin-top: .5vw"
+            >MatrixBrute is arguably the most powerful analog monophonic synthesizer ever ! As flexible as a modular system but offering presets, MatrixBrute is a dream machine for everyone interested in creating a sound palette of their own.</span>
+          </div>
+        </div>
+        <div class="spacer"></div>
+
+        <div ref="animated-row-1" class="product-row" style="display: flex; flex-direction: column">
+          <div
+            style="height: 100%; background-size: contain;background-position: 20vw; background-image: url('/assets/images/airy/matrix-0.png')"
+          >
+            <div style="width: 42vw; padding-left: 4vw">
+              <span class="product-title">Power beyond the matrix</span>
+            </div>
+          </div>
+          <div style="padding-left: 8vw; padding-right: 8vw; text-align: center;">
+            <p
+              class="product-subtitle"
+            >The heart of the MatrixBrute is its unique modulation matrix that allows you to create your own routings. You can invent a sound palette of your own that will be sure to be heard in any mix !</p>
+            <p
+              class="product-subtitle"
+            >The matrix can also be used to recall your presets. So go on stage, and select any of the 256 presets instantly! No menus or awkward selection screens, everything is at your fingertips.</p>
+            <p
+              class="product-subtitle"
+            >As if it was not enough, the matrix is also made to program and play sequences. This is no ordinary step-sequencer. With its ability to also be used as a modulation source, sounds evolve in melody and texture, just the way you want.</p>
+          </div>
+        </div>
+
+        <div class="spacer"></div>
+
+        <div ref="animated-row-2" class="product-row" style="display: flex; flex-direction: column">
+          <div
+            style="height: 100%; background-size: contain;background-position: -36vw; background-image: url('/assets/images/airy/matrix-1.png')"
+          >
+            <div style="width: 42vw; margin-left: auto; padding-right: 4vw; text-align: right">
+              <p class="product-title">Sound beyond the power</p>
+              <p
+                class="product-subtitle"
+                style="margin-top: 15vw; margin-left: 5vw"
+              >The heart of the MatrixBrute is its unique modulation matrix that allows you to create your own routings. You can invent a sound palette of your own that will be sure to be heard in any mix !</p>
+            </div>
+          </div>
+          <div style="padding-left: 8vw; padding-right: 8vw; text-align: center;">
+            <p
+              class="product-subtitle"
+            >The matrix can also be used to recall your presets. So go on stage, and select any of the 256 presets instantly! No menus or awkward selection screens, everything is at your fingertips.</p>
+            <p
+              class="product-subtitle"
+            >As if it was not enough, the matrix is also made to program and play sequences. This is no ordinary step-sequencer. With its ability to also be used as a modulation source, sounds evolve in melody and texture, just the way you want.</p>
+          </div>
+        </div>
+
+        <div class="spacer"></div>
+
+        <span class="product-title">Sound Corner</span>
+      </div>
     </div>
   </div>
 </template>
@@ -18,14 +79,16 @@ export default {
       (entries, observer) => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
-            entry.target.classList.add("view-subtitle")
+            entry.target.classList.add("product-row-animation");
             observer.unobserve(entry.target);
           }
         });
-      }
+      },
+      { rootMargin: "-30%" }
     );
-
-    intersectionObserver.observe(this.$refs["subtitle"]);
+    Object.keys(this.$refs)
+      .filter(ref => ref.includes("animated-row"))
+      .forEach(ref => intersectionObserver.observe(this.$refs[ref]));
   }
 };
 </script>
@@ -41,8 +104,13 @@ export default {
   right: 0;
   width: 23vw;
   height: 100vh;
+  z-index: 9;
 
   background: #171717;
+}
+
+.spacer {
+  height: 12vw;
 }
 
 .view {
@@ -57,22 +125,67 @@ export default {
   letter-spacing: 3vw;
   text-transform: uppercase;
 
-  animation-duration: 6s;
+  animation-duration: 4s;
   animation-name: letter-spacing;
 }
-.view-subtitle {
-  font-size: 5vw;
+
+.product-row {
+  height: 45vw;
+  opacity: 0;
+}
+.product-row-animation {
+  opacity: 1;
 
   animation-duration: 1s;
   animation-name: sliding-up;
+}
+.product-title {
+  font-size: 5vw;
+  line-height: 6vw;
+}
+.product-subtitle {
+  font-size: 1.3vw;
+  line-height: 1.6vw;
+}
+.product-desc {
+  padding-left: 8vw;
+  padding-right: 8vw;
+
+  font-size: 1.1vw;
+  line-height: 1.4vw;
+
+  text-align: center;
+}
+.product-row-right {
+  width: 100%;
+  height: 40vw;
+
+  padding-left: 5vw;
+
+  background-size: 100vw;
+
+  display: flex;
+}
+.product-row-left {
+  width: 100%;
+  height: 40vw;
+
+  padding-left: 5vw;
+
+  background-size: 100vw;
+
+  display: flex;
+  flex-direction: row-reverse;
 }
 
 @keyframes sliding-up {
   from {
     margin-top: 6vw;
+    opacity: 0;
   }
   to {
     margin-top: 0vw;
+    opacity: 1;
   }
 }
 @keyframes letter-spacing {
